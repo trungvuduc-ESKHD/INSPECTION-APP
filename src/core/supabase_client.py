7bff906ef3289759f23dd7f708943b248ddfd88c
trungvu204 <trung.vuduc@eurofinsasia.com>
2025-07-07 15:10:44 +0700

    Create supabase_client.py

---
+++ src/core/supabase_client.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+from supabase import create_client, Client
+
+@st.cache_resource
+def init_supabase_connection():
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["key"]
+    return create_client(url, key)
+
+supabase: Client = init_supabase_connection()
