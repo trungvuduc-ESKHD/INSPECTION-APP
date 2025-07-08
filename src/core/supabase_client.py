d9f2332cc8f1308c2a3401964ebf9ef23f2416ae
trungvu204 <trung.vuduc@eurofinsasia.com>
2025-07-08 15:46:16 +0700

    Update supabase_client.py

--- src/core/supabase_client.py
+++ src/core/supabase_client.py
@@ -13,3 +13,6 @@ supabase: Client = init_supabase_connection()
 def get_users():
     response = supabase.table("users").select("*").execute()
     return response.data
+def save_users(users):
+    for user in users:
+        supabase.table("users").insert(user).execute()
